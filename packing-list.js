889f654ae64ac9dbf055638292ca8b42c196d003
Armin Glavovic <glavovic.armin@gmail.com>
2021-10-22 08:09:56 +0000

    changed to a while loop

---
+++ packing-list.js
@@ -0,0 +1,8 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "cups", "cooking utensils"];
+
+console.log("kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
+}
